fe7db31511530ef416b30f678e849b35a9d5026b
Alex <zhe009@hotmail.com>
2026-05-29 19:07:49 +0800

    Fix CSP compliance: use st:adjunct for CSS/JS, add Jenkinsfile configurations, fix unit test
    - Replace inline style/script in header.jelly with st:adjunct mechanism
    - Move CSS/JS to adjuncts directory for CSP compliance
    - Remove duplicate header.jelly with inline content
    - Remove webapp directory (resources moved to adjuncts)
    - Add configurations parameter to Jenkinsfile buildPlugin call
    - Fix ImagePreviewPluginTest to use @WithJenkins/JenkinsRule

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,1 +1,4 @@
-buildPlugin(jenkinsVersions: [null, '2.528.3'])
+buildPlugin(configurations: [
+    [platform: 'linux', jdk: '17'],
+    [platform: 'windows', jdk: '17'],
+], jenkinsVersions: [null, '2.528.3'])
--- src/test/java/io/jenkins/plugins/artifactimagepreview/ImagePreviewPluginTest.java
+++ src/test/java/io/jenkins/plugins/artifactimagepreview/ImagePreviewPluginTest.java
@@ -2,8 +2,11 @@ package io.jenkins.plugins.artifactimagepreview;
 
 import hudson.model.PageDecorator;
 import org.junit.jupiter.api.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
 import static org.junit.jupiter.api.Assertions.*;
 
+@WithJenkins
 class ImagePreviewPluginTest {
 
     @Test
@@ -13,7 +16,7 @@ class ImagePreviewPluginTest {
     }
 
     @Test
-    void testExtension() {
+    void testExtension(JenkinsRule jenkins) {
         assertTrue(PageDecorator.all().stream()
                 .anyMatch(d -> d instanceof ImagePreviewPlugin));
     }
