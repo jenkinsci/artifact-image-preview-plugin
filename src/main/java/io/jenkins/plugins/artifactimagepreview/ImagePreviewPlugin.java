f1f4725953d19913ad888132ea768db92b7ae924
Alex <zhe009@hotmail.com>
2026-05-31 23:33:55 +0800

    Restrict adjunct to job pages only

--- src/main/java/io/jenkins/plugins/artifactimagepreview/ImagePreviewPlugin.java
+++ src/main/java/io/jenkins/plugins/artifactimagepreview/ImagePreviewPlugin.java
@@ -1,7 +1,11 @@
 package io.jenkins.plugins.artifactimagepreview;
 
 import hudson.Extension;
+import hudson.model.Job;
 import hudson.model.PageDecorator;
+import org.kohsuke.stapler.Ancestor;
+import org.kohsuke.stapler.Stapler;
+import org.kohsuke.stapler.StaplerRequest2;
 
 @Extension
 public class ImagePreviewPlugin extends PageDecorator {
@@ -13,5 +17,14 @@ public class ImagePreviewPlugin extends PageDecorator {
     @Override
     public String getDisplayName() {
         return "Image Artifact Preview";
+    }
+
+    public boolean isOnJobPage() {
+        StaplerRequest2 request = Stapler.getCurrentRequest2();
+        if (request == null) {
+            return false;
+        }
+        Ancestor ancestor = request.findAncestor(Job.class);
+        return ancestor != null && ancestor.getObject() instanceof Job<?, ?>;
     }
 }
